c0e6b913b5ce462e26dc7955df7f9f90fffd1f0a
SpencerO921 <ols24029@byui.edu>
2025-04-29 23:52:37 -0600

    Complete assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a program to compute the area of a circle.
+
+        //Get the radius of the circle from the user
+        Console.Write("Please enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        //Compute the area of the circle
+        double area = Math.PI * radius * radius;
+
+        //Display the Area
+        Console.WriteLine($"Area of the circle: {area}");
+
+
+
+        //Not needed
+        int x = 3;
+        string s = "Goodbye";
+        float f = 3.14F;
+        double d = 5.21;
+        long n = 25;
+        Console.WriteLine($"Hello, {s} Sandbox World! {x} {f} {d} {n}");
     }
 }
