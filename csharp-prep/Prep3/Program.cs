bcea4d167df202a34d0330714823c63f5fd1b63d
SpencerO921 <ols24029@byui.edu>
2025-05-01 22:33:00 -0600

    finished number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int intMagicNumber = randomGenerator.Next(1,101);
+       // Console.WriteLine("Hello Prep3 World!");
+      //  Console.Write("What is the magic number?");
+      //  string textMagicNumber = Console.ReadLine();
+        //int intMagicNumber = int.Parse(textMagicNumber);
+        //int intNumberGuess = null;
+        int intNumberGuess = 0;
+
+        while (intNumberGuess != intMagicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string textGuess = Console.ReadLine();
+            intNumberGuess = int.Parse(textGuess);
+            if (intNumberGuess > intMagicNumber)
+            {
+                Console.WriteLine("Magic Number is Lower.");
+            }
+            else if (intNumberGuess < intMagicNumber)
+            {
+                Console.WriteLine("Magic Number is Higher");
+            }
+            else if (intNumberGuess == intMagicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
+    //In class
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        int number = PromptUserNumber();
+        Console.WriteLine($"Your number is: {number}");
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        Console.ReadLine();
+        int number = int.Parse(Console.ReadLine());
+        return number;
     }
 }
